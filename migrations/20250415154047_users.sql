2357f8eeb0ef21ce8d1a9daf90fd09bbfc9515aa
untemi <94703538+andro404-MC@users.noreply.github.com>
2025-04-27 23:57:09 +0100

    switching to format provided by drawdb

--- migrations/20250415154047_users.sql
+++ migrations/20250415154047_users.sql
@@ -1,12 +1,14 @@
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT NOT NULL,
-  passhash TEXT NOT NULL,
-  firstname TEXT NOT NULL,
-  lastname TEXT,
-  phone TEXT,
-  email TEXT,
-  pfp_file TEXT
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" INTEGER NOT NULL UNIQUE,
+	"username" TEXT NOT NULL,
+	"passhash" TEXT NOT NULL,
+	"firstname" TEXT NOT NULL,
+	"lastname" TEXT,
+	"phone" TEXT,
+	"email" TEXT,
+	"pfp_file" TEXT,
+	PRIMARY KEY("id")
 );
 
-CREATE UNIQUE INDEX idx_users_username ON users (username);
+CREATE UNIQUE INDEX IF NOT EXISTS "users_index_0"
+ON "users" ("username");
