1beaa7f000f1c7833e510e6828e7c4d7dc2f6237
untemi <94703538+andro404-MC@users.noreply.github.com>
2025-05-08 17:38:00 +0100

    change: in code migration

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,8 @@
 use chrono::NaiveDate;
+use include_dir::{Dir, include_dir};
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite_migration::Migrations;
 use serde::Serialize;
 use std::sync::LazyLock;
 
@@ -36,9 +38,16 @@ pub struct District {
     pub name: String,
 }
 
+static MIGRATIONS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/migrations");
+
 pub static POOL: LazyLock<Pool<SqliteConnectionManager>> = LazyLock::new(|| {
     let manager = SqliteConnectionManager::file("app.db");
-    r2d2::Pool::new(manager).unwrap()
+    let pool = r2d2::Pool::new(manager).unwrap();
+
+    let migrations = Migrations::from_directory(&MIGRATIONS_DIR).unwrap();
+    migrations.to_latest(&mut pool.get().unwrap()).unwrap();
+
+    pool
 });
 
 pub static DISTRICTS: LazyLock<Box<[District]>> = LazyLock::new(|| {
